eb782293b02f1618582660e3bbd7842842798d0b
Awwal2007 <awwalsaminu9@gmail.com>
2025-08-06 14:14:01 +0100

    dsdfsf

--- Routes/messageRouter.js
+++ Routes/messageRouter.js
@@ -3,17 +3,19 @@ const messageRouter = express.Router()
 
 const {
     createUserMessage,
-    getAllMessages,
+    getAllAdminMessagesFromAdmin,
     getMyMessages,
     getAdminMessages,
+    getAllUserMessages,
     getUserMessageFromAdmin,
     replyToUserMessage} = require("../Controllers/messageController")
 const isLoggedIn = require("../Middlewares/isLoggedIn")
 const isAdmin = require("../Middlewares/isAdmin")
 
 messageRouter.post('/', isLoggedIn, createUserMessage);
-messageRouter.get('/', isLoggedIn,  getAllMessages);
-messageRouter.get('/user-messages', isLoggedIn, getMyMessages);
+messageRouter.get('/', isLoggedIn,  getAllUserMessages);
+messageRouter.get('/admin/:userId', isLoggedIn,  getAllAdminMessagesFromAdmin);
+messageRouter.get('/user-messages/:userId', isLoggedIn, getMyMessages);
 messageRouter.get('/admin-messages/:userId', isLoggedIn, getAdminMessages);
 messageRouter.get('/admin-user-message/:userId', isLoggedIn, getUserMessageFromAdmin);
 messageRouter.post('/admin-reply', isLoggedIn, isAdmin, replyToUserMessage);
